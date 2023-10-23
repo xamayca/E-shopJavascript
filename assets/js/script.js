180fc105ad7a5ca344ae0c130d2c4a2a91d346e2
xamayca <christopher.hariouat@gmail.com>
2023-10-23 23:15:28 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+// Pour stocké les articles crée //
+let article = [];
+
+
+// Déclaration pour utilisation externe a l'addEventListener //
+let articleName;
+// Récupère le input pour le nom des articles HTML //
+const articleNameHtml = document.getElementById('articleName');
+articleNameHtml.addEventListener('input', () => {
+    //Renvois le texte tapé dans l'input //
+    articleName = articleNameHtml.value;
+
+    console.log(`Article name is: ${articleName}`);
+});
+
+
+// Déclaration pour utilisation externe a l'addEventListener //
+let catName;
+// Récupère le sélecteur de categories HTML //
+const articleSelectCatHtml = document.getElementById('articleSelectCat');
+articleSelectCatHtml.addEventListener('change', () => {
+
+    // Important //
+    //Renvois le texte Html de l'option sélectionnée dans select menu //
+    catName = articleSelectCatHtml.options[articleSelectCatHtml.selectedIndex].text;
+
+    console.log(`Category select is: ${catName}`);
+});
+
+
+const articleAddBtnHtml = document.getElementById('articleAddBtn');
+articleAddBtnHtml.addEventListener('click', () => {
+    article.unshift({ title: articleName, category: catName });
+
+    console.log('Add button is clicked');
+    console.log(article);
+
+    displayArticle();
+});
+
+
+const articleListHtml = document.getElementById('articleList');
+articleListHtml.classList.add('gap-1');
+
+const displayArticle = () => {
+
+    console.log(`Article Name is: ${article[0].title}`);
+    console.log(`Article Category is: ${article[0].category}`);
+
+    //Crée les éléments HTML //
+    //Crée une div //
+    const newDiv = document.createElement("div");
+
+    //Crée un h2 avec le texteContent du nom de l'article //
+    const newTitle = document.createElement("h2");
+    newTitle.textContent = `Article: ${article[0].title}`;
+
+    //Crée une span avec le texteContent de la catégorie de l'article //
+    const newSpan = document.createElement("span");
+    newSpan.textContent = `Catégorie: ${article[0].category}`;
+
+    // Attache la div crée a la div HTML id: articleList //
+    articleListHtml.appendChild(newDiv);
+    // Attache le h2 crée a la div crée (newDiv) //
+    newDiv.appendChild(newTitle);
+    // Attache le span crée a la div crée (newDiv) //
+    newDiv.appendChild(newSpan);
+
+    // Ajoute les style de la fonction (addStyle) aux arguments newDiv et newSpan //
+    addStyleToArticle(newDiv, newSpan);
+}
+
+
+// Fonction pour ajouter des styles a un article (addStyle) //
+const addStyleToArticle = (div, span) => {
+    // Style des div crée //
+    div.classList.add('d-flex');
+    div.classList.add('flex-col');
+    div.classList.add('justify-between');
+    div.classList.add('items-center');
+
+    div.classList.add('txt-white');
+    div.classList.add('txt-center');
+    div.classList.add('fw-bold');
+
+    div.classList.add('w-75');
+    div.classList.add('p-05');
+    div.classList.add('bg-light');
+
+    // Style des span crée //
+    span.classList.add('bg-dark');
+    span.classList.add('p-05');
+    span.classList.add('w-75');
+}
