4557e0f0a0ead2767bed29d075eb16405efe11bd
xamayca <Christopher.hariouat@gmail.com>
2023-10-24 10:35:47 +0200

    some fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,12 @@
 let article = [];
 
 
-// Déclaration pour utilisation externe a l'addEventListener //
-let articleName;
-// Récupère le input pour le nom des articles HTML //
 const articleNameHtml = document.getElementById('articleName');
-articleNameHtml.addEventListener('input', () => {
-    //Renvois le texte tapé dans l'input //
-    articleName = articleNameHtml.value;
 
-    console.log(`Article name is: ${articleName}`);
-});
 
 // CODE POUR RÉCUPÉRER LA SELECTION DU MENU DÉROULANT //
-// Déclaration pour utilisation externe a l'addEventListener //
-let categoryName;
 // Récupère le sélecteur de categories HTML //
 const articleSelectCatHtml = document.getElementById('articleSelectCat');
-articleSelectCatHtml.addEventListener('change', () => {
-
-    console.log(`Category select is: ${categoryName}`);
-
-    // /!\ Important /!\ //
-    //Renvois le texte contenu dans l'option sélectionnée du select menu //
-    categoryName = articleSelectCatHtml.options[articleSelectCatHtml.selectedIndex].text;
-
-});
-
 
 // CODE POUR AJOUTER UN ARTICLE //
 // Récupère le boutons d'ajout d'article HTML //
@@ -37,9 +17,12 @@ articleAddBtnHtml.addEventListener('click', () => {
     console.log('Add button is clicked');
     console.log(article);
 
-    // Ajoute au tableau article le titre récupérer et la categories sélectionné //
-    article.unshift({ title: articleName, category: categoryName });
+    const newArticle = article.filter(el => el.title == articleNameHtml.value);
 
+    if (newArticle.length == 0) {
+        // Ajoute au tableau article le titre récupérer et la categories sélectionné //
+        article.unshift({ title: articleNameHtml.value, category: articleSelectCatHtml.value });
+    }
     // Appel de la fonction display//
     displayArticle();
 
@@ -52,30 +35,35 @@ const articleListHtml = document.getElementById('articleList');
 // Fonction pour afficher les articles (displayArticle)) //
 const displayArticle = () => {
 
+    // /!\ IMPORTANT: VIDE LA LISTE D'ARTICLE HTML /!\ //
+    articleListHtml.innerHTML = '';
+
     console.log(`Article Name is: ${article[0].title}`);
     console.log(`Article Category is: ${article[0].category}`);
 
+    for (let i = 0; i < article.length; i++) {
+        //Crée les éléments HTML //
+        //Crée une div //
+        const articleDiv = document.createElement("div");
 
-    //Crée les éléments HTML //
-    //Crée une div //
-    const newDiv = document.createElement("div");
+        //Crée un h2 avec le texteContent du nom de l'article //
+        const articleTitle = document.createElement("h2");
+        articleTitle.textContent = `Article: ${article[i].title}`;
 
-    //Crée un h2 avec le texteContent du nom de l'article //
-    const newTitle = document.createElement("h2");
-    newTitle.textContent = `Article: ${article[0].title}`;
+        //Crée une span avec le texteContent de la catégorie de l'article //
+        const articleSpan = document.createElement("span");
+        articleSpan.textContent = `Catégorie: ${article[i].category}`;
 
-    //Crée une span avec le texteContent de la catégorie de l'article //
-    const newSpan = document.createElement("span");
-    newSpan.textContent = `Catégorie: ${article[0].category}`;
+        // Attache le h2 crée a la div crée (newDiv) //
+        articleDiv.appendChild(articleTitle);
+        // Attache le span crée a la div crée (newDiv) //
+        articleDiv.appendChild(articleSpan);
+        // Ajoute les style de la fonction (addStyle) aux arguments newDiv et newSpan //
+        addStyleToArticle(articleDiv, articleSpan);
+        // Attache la div crée a la div HTML id: articleList //
+        articleListHtml.appendChild(articleDiv);
+    }
 
-    // Attache la div crée a la div HTML id: articleList //
-    articleListHtml.appendChild(newDiv);
-    // Attache le h2 crée a la div crée (newDiv) //
-    newDiv.appendChild(newTitle);
-    // Attache le span crée a la div crée (newDiv) //
-    newDiv.appendChild(newSpan);
-    // Ajoute les style de la fonction (addStyle) aux arguments newDiv et newSpan //
-    addStyleToArticle(newDiv, newSpan);
 
 }
 
